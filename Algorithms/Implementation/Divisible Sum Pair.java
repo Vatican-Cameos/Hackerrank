3c75a418a10caf7643f0adcb56e71744332b4ea4
Pavan <pavanvenkatesh94@gmail.com>
2016-09-10 18:20:26 +0530

    implementation added

---
+++ Algorithms/Implementation/Divisible Sum Pair.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int count = 0;
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        for(int i =0; i < n-1; i++){
+            for(int j = i+1; j<n; j++){
+                int sum = a[i] + a[j];
+                if(sum%k == 0)
+                    count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
